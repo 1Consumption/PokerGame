607da15dd041ebbbf95afa35ff7edc21d6a51073
1Consumption <hanseop95@gmail.com>
2020-12-31 17:22:38 +0900

    chore: 프로젝트 초기화

---
+++ PokerGame/PokerGame/SceneDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  SceneDelegate.swift
+//  PokerGame
+//
+//  Created by 신한섭 on 2020/12/31.
+//
+
+import UIKit
+
+final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+}
---
+++ PokerGame/PokerGame/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  PokerGame
+//
+//  Created by 신한섭 on 2020/12/31.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
